d63aeada312627d5f4cd9d89684cb25b46a2ddf0
Kameha1011 <omarcitofc2001@gmail.com>
2024-06-12 00:32:57 -0300

    Añadida documentacion para funciones y algnos comentarios

--- frontend/assets/js/funciones.js
+++ frontend/assets/js/funciones.js
@@ -1,3 +1,9 @@
+/**
+ * Renderiza una grilla de personajes en la página web basada en los datos de personajes proporcionados.
+ *
+ * @param {Array} personajes - Un array de objetos de personajes que contienen imagen, nombre e id.
+ * @return {void} Esta función no devuelve un valor.
+ */
 export const dibujar_grilla_personajes = (personajes) => {
   const contenedor = document.querySelector(".grillaPersonajes");
   let esqueleto_contenedor = "";
@@ -19,14 +25,24 @@ export const dibujar_grilla_personajes = (personajes) => {
 /**
  * Asigna el valor de el atributo 'data-id' del target el evento al parámetro 'id' .
  *
- * @param {Event} event - The event object.
- * @param {number} id - The parameter to assign the 'data-id' value to.
- * @return {void} This function does not return a value.
+ * @param {Event} event - El objeto event.
+ * @param {number} id - El ID a asignarle a el atributo 'data-id'.
+ * @return {void} La función no devuelve ningun valor.
  */
 export const obtener_id_personaje = (event) => {
   return parseInt(event.target.getAttribute("data-id"));
 };
 
+/**
+ * Una función que cambia los estados de los botones basados en los IDs proporcionados.
+ *
+ * @param {any} id_1 - El primer ID.
+ * @param {any} id_2 - El segundo ID.
+ * @param {HTMLElement} boton_1 - El primer elemento de botón.
+ * @param {HTMLElement} boton_2 - El segundo elemento de botón.
+ * @param {HTMLElement} boton_iniciar - El elemento de botón de inicio.
+ * @return {void} Esta función no devuelve ningún valor.
+ */
 export const switch_botones = (id_1, id_2, boton_1, boton_2, boton_iniciar) => {
   if (id_1 && !id_2) {
     boton_1.disabled = true;
@@ -37,6 +53,13 @@ export const switch_botones = (id_1, id_2, boton_1, boton_2, boton_iniciar) => {
   }
 };
 
+/**
+ * Actualiza el contenedor con los datos del personaje proporcionados.
+ *
+ * @param {HTMLElement} contenedor - El elemento contenedor que se actualizará.
+ * @param {Object} datos_personaje - Los datos del personaje a mostrar.
+ * @return {void} Esta función no devuelve un valor.
+ */
 export const actualizar_contendor_personaje = (contenedor, datos_personaje) => {
   const name = contenedor.querySelector(".personajes__nombre");
   const imagen = contenedor.querySelector(".personajes__img");
@@ -45,6 +68,15 @@ export const actualizar_contendor_personaje = (contenedor, datos_personaje) => {
   name.innerText = datos_personaje.nombre;
 };
 
+/**
+ * Setea los query strings para la ruta dada and con los IDs de personajes, y actualiza el href del botón.
+ *
+ * @param {HTMLElement} boton - El boton a actualizar.
+ * @param {string} route - La ruta base para construir la URL.
+ * @param {number} id_personaje_1 - El ID del primer personaje.
+ * @param {number} id_personaje_2 - El ID del segundo personaje.
+ * @return {void} La función no devuelve ningun valor.
+ */
 export const setear_query_strings = (
   boton,
   route,
--- frontend/assets/js/seleccion-personajes.js
+++ frontend/assets/js/seleccion-personajes.js
@@ -6,11 +6,12 @@ import {
   actualizar_contendor_personaje,
   setear_query_strings,
   actualizar_texto,
-  fetch_personajes
+  fetch_personajes,
 } from "./funciones.js";
 
 const personajes = await fetch_personajes();
 
+// Referencias HTML
 const contenedor_personaje_1 = document.querySelector(".personajes__1");
 const contenedor_personaje_2 = document.querySelector(".personajes__2");
 const boton_confirmar_pj1 = document.querySelector("#confirmar_personaje_1");
@@ -21,13 +22,15 @@ const texto_status = document.querySelector(".personajes__text");
 // Variables de estado
 let id_personaje_1, id_personaje_2, id_personaje_elegido;
 
+// Inicializar html
 actualizar_contendor_personaje(contenedor_personaje_1, personajes[0]);
 actualizar_contendor_personaje(contenedor_personaje_2, personajes[1]);
-
 dibujar_grilla_personajes(personajes);
 const contenedores_personajes = document.querySelectorAll(
   ".grillaPersonajes__thumbnail--img"
 );
+
+// Asignar eventos
 for (const contenedor of contenedores_personajes) {
   contenedor.addEventListener(
     "click",
@@ -65,6 +68,11 @@ boton_confirmar_pj2.addEventListener("click", () => {
     const personaje = personajes.filter((pj) => pj.id === id_personaje_2)[0];
     actualizar_contendor_personaje(contenedor_personaje_2, personaje);
     actualizar_texto(texto_status, "Personajes Listos. ¡A pelear!");
-    setear_query_strings(boton_iniciar, "./juego.html", id_personaje_1, id_personaje_2);
+    setear_query_strings(
+      boton_iniciar,
+      "./juego.html",
+      id_personaje_1,
+      id_personaje_2
+    );
   }
 });
