244ce281fce4143dc4a7e0de0498e722bcfc28f6
Kameha1011 <omarcitofc2001@gmail.com>
2024-06-11 21:08:23 -0300

    Creada la logica de la ventana de seleccionar personajes

---
+++ frontend/assets/js/funciones.js
@@ -0,0 +1,54 @@
+export const dibujar_grilla_personajes = (personajes) => {
+  const contenedor = document.querySelector(".grillaPersonajes");
+  let esqueleto_contenedor = "";
+  for (const personaje of personajes) {
+    esqueleto_contenedor += `
+           <div class="grillaPersonajes__thumbnail">
+            <img
+              src="${personaje.imagen}"
+              alt="${personaje.nombre}"
+              data-id="${personaje.id}"
+              class="grillaPersonajes__thumbnail--img"
+            />
+          </div>
+          `;
+  }
+  contenedor.innerHTML = esqueleto_contenedor;
+};
+
+/**
+ * Asigna el valor de el atributo 'data-id' del target el evento al parámetro 'id' .
+ *
+ * @param {Event} event - The event object.
+ * @param {number} id - The parameter to assign the 'data-id' value to.
+ * @return {void} This function does not return a value.
+ */
+export const obtener_id_personaje = (event) => {
+  return parseInt(event.target.getAttribute("data-id"));
+};
+
+export const switch_botones = (id_1, id_2, boton_1, boton_2, boton_iniciar) => {
+  if (id_1 && !id_2) {
+    boton_1.disabled = true;
+    boton_2.disabled = false;
+  } else if (id_1 && id_2) {
+    boton_2.disabled = true;
+    boton_iniciar.disabled = false;
+  }
+};
+
+export const actualizar_contendor_personaje = (contenedor, datos_personaje) => {
+  const name = contenedor.querySelector(".personajes__nombre");
+  const imagen = contenedor.querySelector(".personajes__img");
+  imagen.src = datos_personaje.imagen;
+  name.innerText = datos_personaje.nombre;
+};
+
+export const setear_query_strings = (boton, route, id_personaje_1, id_personaje_2) => {
+    const url = `${route}?personaje_1=${id_personaje_1}&personaje_2=${id_personaje_2}`;
+    boton.href = url;
+}
+
+export const actualizar_texto = (contenedor, texto) => {
+    contenedor.innerText = texto;
+  };
---
+++ frontend/assets/js/seleccion-personajes.js
@@ -0,0 +1,66 @@
+import { personajes } from "./personajes.js";
+import {
+  dibujar_grilla_personajes,
+  switch_botones,
+  obtener_id_personaje,
+  actualizar_contendor_personaje,
+  setear_query_strings,
+  actualizar_texto
+} from "./funciones.js";
+
+const contenedor_personaje_1 = document.querySelector(".personajes__1");
+const contenedor_personaje_2 = document.querySelector(".personajes__2");
+const boton_confirmar_pj1 = document.querySelector("#confirmar_personaje_1");
+const boton_confirmar_pj2 = document.querySelector("#confirmar_personaje_2");
+const boton_iniciar = document.querySelector("#iniciar_juego");
+const texto_status = document.querySelector(".personajes__text");
+
+
+
+// Variables de estado
+let id_personaje_1, id_personaje_2, id_personaje_elegido;
+
+dibujar_grilla_personajes(personajes);
+const contenedores_personajes = document.querySelectorAll(
+  ".grillaPersonajes__thumbnail--img"
+);
+for (const contenedor of contenedores_personajes) {
+  contenedor.addEventListener(
+    "click",
+    (event) => (id_personaje_elegido = obtener_id_personaje(event))
+  );
+}
+
+boton_confirmar_pj1.addEventListener("click", () => {
+  id_personaje_1 = id_personaje_elegido;
+  id_personaje_elegido = null;
+  switch_botones(
+    id_personaje_1,
+    id_personaje_2,
+    boton_confirmar_pj1,
+    boton_confirmar_pj2,
+    boton_iniciar
+  );
+  if (id_personaje_1) {
+    const personaje = personajes.filter((pj) => pj.id === id_personaje_1)[0];
+    actualizar_contendor_personaje(contenedor_personaje_1, personaje);
+    actualizar_texto(texto_status, "Jugador 2, escoja su personaje");
+  }
+});
+boton_confirmar_pj2.addEventListener("click", () => {
+  id_personaje_2 = id_personaje_elegido;
+  id_personaje_elegido = null;
+  switch_botones(
+    id_personaje_1,
+    id_personaje_2,
+    boton_confirmar_pj1,
+    boton_confirmar_pj2,
+    boton_iniciar
+  );
+  if (id_personaje_1 && id_personaje_2) {
+    const personaje = personajes.filter((pj) => pj.id === id_personaje_2)[0];
+    actualizar_contendor_personaje(contenedor_personaje_2, personaje);
+    actualizar_texto(texto_status, "Personajes Listos. ¡A pelear!");
+    setear_query_strings(boton_iniciar, "./juego.html", id_personaje_1, id_personaje_2);
+  }
+});
