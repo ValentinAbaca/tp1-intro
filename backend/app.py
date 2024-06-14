7cb0055cf93ca3298179385f23fa91ca9c34d5b4
Valentin Abaca <vabaca@fi.uba.ar>
2024-06-14 17:56:12 -0300

    Modularizado codigo a nuevo archivo funciones.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from models import db, Personajes, Ataques, PersonajesAtaques
+from models import db
 from dotenv import load_dotenv
 import os
+
+from funciones import obtener_data_personajes, obtener_data_personaje_id, obtener_data_ataques, obtener_data_ataque_id, \
+    crear_nuevo_personaje, crear_nuevo_ataque, borrar_personaje, borrar_ataque, modificar_personaje, modificar_ataque
 
 load_dotenv() #Carga variables de ambiente del archivo .env
 DATABASE_USER = os.getenv("DATABASE_USER")
@@ -20,288 +23,86 @@ def main_route():
 
 @app.route('/personajes', methods=['GET'])
 def obtener_personajes():
-    try:
-        personajes = Personajes.query.all()
-        personajes_data = []
-        for personaje in personajes:
-            personaje_data = {
-                'id': personaje.id,
-                'nombre': personaje.nombre,
-                'vida': personaje.vida,
-                'ki': personaje.ki,
-                'descripcion': personaje.descripcion,
-                'raza': personaje.raza,
-                'imagen': personaje.imagen,
-                'ataques': []
-            }
+    personajes_data = obtener_data_personajes()
 
-            ataques = PersonajesAtaques.query.filter_by(id_personaje=personaje.id).all()
-            
-            for ataque in ataques:
-                ataque_data = {
-                    'id': ataque.ataque.id,
-                    'nombre': ataque.ataque.nombre,
-                    'costo_ki': ataque.ataque.costo_ki,
-                    'dano_max': ataque.ataque.dano_max,
-                    'dano_min': ataque.ataque.dano_min
-                }
-                
-                personaje_data['ataques'].append(ataque_data)
-            personajes_data.append(personaje_data)
-        return jsonify(personajes_data)
-    except Exception as error:
-        print(f"Error al obtener personajes: {str(error)}")
-        return jsonify({'message': 'Personajes no encontrados'}), 500
-    
+    if not personajes_data:
+        return jsonify({'Error': 'Personajes no encontrados'}), 404
+
+    return jsonify(personajes_data)
 
 @app.route('/personajes/<id>', methods=['GET'])
 def obtener_personaje_id(id):
-    try:
-        personaje = Personajes.query.get(id)
-        personaje_data = {
-            'id': personaje.id,
-            'nombre': personaje.nombre,
-            'vida': personaje.vida,
-            'ki': personaje.ki,
-            'descripcion': personaje.descripcion,
-            'raza': personaje.raza,
-            'imagen': personaje.imagen,
-            'ataques': []
-        }
-        ataques = PersonajesAtaques.query.filter_by(id_personaje=personaje.id).all()
-        for ataque in ataques:
-            ataque_data = {
-                'id': ataque.ataque.id,
-                'nombre': ataque.ataque.nombre,
-                'costo_ki': ataque.ataque.costo_ki,
-                'dano_max': ataque.ataque.dano_max,
-                'dano_min': ataque.ataque.dano_min
-            }
-            personaje_data['ataques'].append(ataque_data)
-        return jsonify(personaje_data)
-    except Exception as error:
-        print(f"Error al obtener personaje: {str(error)}")
-        return jsonify({'message': 'Personaje no encontrado'}), 500
+    personaje_data = obtener_data_personaje_id(id)
+    if not personaje_data:
+        return jsonify({'Error': 'Personaje no encontrado'}), 404
+    return jsonify(personaje_data)
 
-@app.route('/ataques')
+@app.route('/ataques', methods=['GET'])
 def obtener_ataques():
-    try:
-        ataques = Ataques.query.all()
-        ataques_data = []
-        for ataque in ataques:
-            ataque_data = {
-                'id': ataque.id,
-                'nombre': ataque.nombre,
-                'costo_ki': ataque.costo_ki,
-                'dano_max': ataque.dano_max,
-                'dano_min': ataque.dano_min
-            }
-            ataques_data.append(ataque_data)
-        return jsonify(ataques_data)
-    except Exception as error:
-        print(f"Error al obtener ataques: {str(error)}")
-        return jsonify({'message': 'Ataques no encontrados'}), 500
+    ataques_data = obtener_data_ataques()
+    if not ataques_data:
+        return jsonify({'Error': 'Ataques no encontrados'}), 404
+    return jsonify(ataques_data)
 
-
-@app.route('/ataques/<id>')
+@app.route('/ataques/<id>', methods=['GET'])
 def obtener_ataque_id(id):
-    try:
-        ataque = Ataques.query.get(id)
-        ataque_data = {
-            'id': ataque.id,
-            'nombre': ataque.nombre,
-            'costo_ki': ataque.costo_ki,
-            'dano_max': ataque.dano_max,
-            'dano_min': ataque.dano_min
-        }
-        return jsonify(ataque_data)
-    except Exception as error:
-        print(f"Error al obtener ataque: {str(error)}")
-        return jsonify({'message': 'Ataque no encontrado'}), 500
+    ataque_data = obtener_data_ataque_id(id)
+    if not ataque_data:
+        return jsonify({'Error': 'Ataque no encontrado'}), 404
+    return jsonify(ataque_data)
 
 @app.route('/nuevo_personaje', methods=['POST'])
 def crear_personaje():
-    try:
-        id_max = Personajes.query.order_by(Personajes.id.desc()).first()
-        id = id_max.id + 1
-
-        data = request.form
-        nuevo_nombre = data['nombre']
-
-        verificar_personaje = Personajes.query.filter_by(nombre=nuevo_nombre).first()
-        if verificar_personaje:
-            return jsonify({'message': 'El personaje ya existe'}), 400
-
-        nueva_vida = data['vida']
-        nuevo_ki = data['ki']
-        nueva_descripcion = data['descripcion']
-        nueva_raza = data['raza']
-        
-        nueva_imagen = request.files['imagen']
-        nueva_imagen.save(f"static/img/{nueva_imagen.filename}")
-        print(f"Imagen guardada en: static/img/{nueva_imagen.filename}")
-        path = f"http://localhost:5000/static/img/{nueva_imagen.filename}"
-
-        personaje = Personajes(id=id, nombre=nuevo_nombre, vida=nueva_vida, ki=nuevo_ki, descripcion=nueva_descripcion, raza=nueva_raza, imagen=path)
-        db.session.add(personaje)
-        db.session.commit()
-
-        ataques = data['ataques'].split(',')
-        for ataque in ataques:
-            ataque_id = int(ataque)
-            max_id_relacion = PersonajesAtaques.query.order_by(PersonajesAtaques.id.desc()).first()
-            id_relacion = max_id_relacion.id + 1
-            personaje_ataque = PersonajesAtaques(id=id_relacion, id_personaje=id, id_ataque=ataque_id)
-            db.session.add(personaje_ataque)
-            db.session.commit()
-
-        nuevos_ataques = []
-        for ataque in ataques:
-            ataque_id = int(ataque)
-            ataque = Ataques.query.get(ataque_id)
-            ataque_data = {
-                'id': ataque.id,
-                'nombre': ataque.nombre,
-                'costo_ki': ataque.costo_ki,
-                'dano_max': ataque.dano_max,
-                'dano_min': ataque.dano_min
-            }
-            nuevos_ataques.append(ataque_data)
-
-        return jsonify({"personaje" : {"id" : personaje.id, 
-                                        "nombre" : personaje.nombre, 
-                                        "vida" : personaje.vida, 
-                                        "ki" : personaje.ki, 
-                                        "descripcion" : personaje.descripcion,
-                                        "raza" : personaje.raza, 
-                                        "imagen" : personaje.imagen,
-                                        "ataques" : nuevos_ataques
-                                        }}), 201
-    except Exception as error:
-        print(f"Error al crear personaje: {str(error)}")
-        return jsonify({'message': 'Error al crear personaje'}), 500
+    data = request.form
+    imagen = request.files['imagen']
+    if not imagen:
+        return jsonify({'message': 'Mala request imagen no encontrada'}), 400
+    personaje = crear_nuevo_personaje(data, imagen)
+    if personaje == None:
+        return jsonify({'message': 'El personaje ya existe'}), 400
+    return jsonify({'succes' : True, 'personaje' : personaje}), 201
 
 @app.route('/nuevo_ataque', methods=['POST'])
 def crear_ataque():
-    try:
-        id_max = Ataques.query.order_by(Ataques.id.desc()).first()
-        id = id_max.id + 1
-
-        data = request.form
-        nuevo_nombre = data['nombre']
-
-        verificar_ataque = Ataques.query.filter_by(nombre=nuevo_nombre).first()
-        if verificar_ataque:
-            return jsonify({'message': 'El ataque ya existe'}), 400
-
-        nuevo_costo_ki = data['costo_ki']
-        nuevo_dano_max = data['dano_max']
-        nuevo_dano_min = data['dano_min']
-
-        ataque = Ataques(id=id, nombre=nuevo_nombre, costo_ki=nuevo_costo_ki, dano_max=nuevo_dano_max, dano_min=nuevo_dano_min)
-        db.session.add(ataque)
-        db.session.commit()
-
-        return jsonify({"ataque" : {"id" : ataque.id, 
-                                    "nombre" : ataque.nombre, 
-                                    "costo_ki" : ataque.costo_ki, 
-                                    "dano_max" : ataque.dano_max, 
-                                    "dano_min" : ataque.dano_min
-                                    }}), 201
-    except Exception as error:
-        print(f"Error al crear ataque: {str(error)}")
-        return jsonify({'message': 'Error al crear ataque'}), 500
+    data = request.form
+    ataque = crear_nuevo_ataque(data)
+    if not ataque:
+        return jsonify({'message': 'El ataque ya existe'}), 400
+    return jsonify({'succes' : True, 'Ataque': ataque}), 201
 
 @app.route('/borrar_personaje/<id>', methods=['DELETE'])
-def borrar_personaje(id):
-    try:
-        personaje_ataques = PersonajesAtaques.query.filter_by(id_personaje=id).all()
-        for personaje_ataque in personaje_ataques:
-            db.session.delete(personaje_ataque)
-            db.session.commit()
-        
-        personaje = Personajes.query.get(id)
+def borrar_personaje_por_id(id):
+    borrar = borrar_personaje(id)
+    if not borrar:
+        return jsonify({'message': 'Personaje no encontrado'}), 404
+    return jsonify({'message': f'Personaje id: {id} borrado correctamente'}), 200
 
-        img_path = personaje.imagen.split('/')[-1]
-        os.remove(f"static/img/{img_path}")
-
-        db.session.delete(personaje)
-        db.session.commit()
-        return jsonify({'message': f'Personaje id: {id} borrado correctamente'}), 200
-
-    except Exception as error:
-        print(f"Error al borrar personaje: {str(error)}")
-        return jsonify({'message': f'Error al borrar personaje {id}'}), 400
-    
 @app.route('/borrar_ataque/<id>', methods=['DELETE'])
-def borrar_ataque(id):
-    try:
-        ataques_personajes = PersonajesAtaques.query.filter_by(id_ataque=id).all()
-        for ataque_personaje in ataques_personajes:
-            db.session.delete(ataque_personaje)
-            db.session.commit()
-        
-        ataque = Ataques.query.get(id)
-        db.session.delete(ataque)
-        db.session.commit()
-        return jsonify({'message': f'Ataque id: {id} borrado correctamente'}), 200
-
-    except Exception as error:
-        print(f"Error al borrar ataque: {str(error)}")
-        return jsonify({'message': f'Error al borrar ataque {id}'}), 500
+def borrar_ataque_por_id(id):
+    borrar = borrar_ataque(id)
+    if not borrar:
+        return jsonify({'message': 'Ataque no encontrado'}), 404
+    return jsonify({'message': f'Ataque id: {id} borrado correctamente'}), 200
 
 @app.route('/modificar_ataque/<id>', methods=['PUT'])
-def modificar_ataque(id):
-    try:
-        ataque = Ataques.query.get(id)
-        data = request.form
-        ataque.nombre = data['nombre']
-        ataque.costo_ki = data['costo_ki']
-        ataque.dano_max = data['dano_max']
-        ataque.dano_min = data['dano_min']
-        db.session.commit()
-        return jsonify({'message': f'Ataque id: {id} modificado correctamente'}), 200
-    except Exception as error:
-        print(f"Error al modificar ataque: {str(error)}")
-        return jsonify({'message': f'Error al modificar ataque {id}'}), 500
+def modificar_ataque_por_id(id):
+    data = request.form
+    ataque = modificar_ataque(id, data)
+    if not ataque:
+        return jsonify({'message': 'Ataque no encontrado'}), 404
+    return jsonify({"Ataque" : ataque}), 200
 
 @app.route('/modificar_personaje/<id>', methods=['PUT'])
-def modificar_personaje(id):
-    try:
-        personaje = Personajes.query.get(id)
-        data = request.form
-
-        if 'imagen' in request.files:
-            os.remove(f"static/img/{personaje.imagen.split('/')[-1]}")
-            nueva_imagen = request.files['imagen']
-            nueva_imagen.save(f"static/img/{nueva_imagen.filename}")
-            print(f"Imagen guardada en: static/img/{nueva_imagen.filename}")
-            path = f"http://localhost:5000/static/img/{nueva_imagen.filename}"
-            personaje.imagen = path
-
-        personaje.nombre = data['nombre']
-        personaje.vida = data['vida']
-        personaje.ki = data['ki']
-        personaje.descripcion = data['descripcion']
-        personaje.raza = data['raza']
-
-        db.session.commit()
-
-        ids_ataques = data['ataques'].split(',')
-        
-        for id_ataque in ids_ataques:
-            if PersonajesAtaques.query.filter_by(id_personaje=id, id_ataque=id_ataque).count() == 0:
-                max_id_relacion = PersonajesAtaques.query.order_by(PersonajesAtaques.id.desc()).first()
-                id_relacion = max_id_relacion.id + 1
-                personaje_ataque = PersonajesAtaques(id=id_relacion, id_personaje=id, id_ataque=id_ataque)
-                db.session.add(personaje_ataque)
-                db.session.commit()
-        
-        return jsonify({'message': f'Personaje id: {id} modificado correctamente'}), 200
-    
-    except Exception as error:
-        print(f"Error al modificar personaje: {str(error)}")
-        return jsonify({'message': f'Error al modificar personaje {id}'}), 500
+def modificar_personaje_por_id(id):
+    data = request.form
+    if 'imagen' in request.files:
+        imagen = request.files['imagen']
+    else:
+        imagen = None 
+    personaje = modificar_personaje(id, data, imagen)
+    if not personaje:
+        return jsonify({'message': 'Personaje no encontrado'}), 404
+    return jsonify({"Personaje" : personaje}), 200
 
 if __name__ == '__main__':
     db.init_app(app)
