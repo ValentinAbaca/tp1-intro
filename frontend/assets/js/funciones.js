e14258cccbc51dfa81c275ac76880efd52c6fb8f
Kameha1011 <omarcitofc2001@gmail.com>
2024-06-14 18:54:12 -0300

    actualizados los jsdocs

--- frontend/assets/js/funciones.js
+++ frontend/assets/js/funciones.js
@@ -1,3 +1,10 @@
+/**
+ * Renderiza la lista de personajes dada en el elemento de contenedor especificado.
+ *
+ * @param {HTMLDivElement} grilla - El elemento de contenedor donde se renderizarán los personajes.
+ * @param {Array<Object>} personajes - La lista de personajes a renderizar.
+ * @return {void} Esta función no devuelve nada.
+ */
 export const renderizar_personajes = (grilla, personajes) => {
   let esqueleto_contenedor = "";
   for (const personaje of personajes) {
@@ -14,11 +21,10 @@ export const renderizar_personajes = (grilla, personajes) => {
 };
 
 /**
- * Asigna el valor de el atributo 'data-id' del target el evento al parámetro 'id' .
+ * Devuelve el ID de un personaje a partir del elemento icono_personaje dado.
  *
- * @param {Event} event - El objeto event.
- * @param {number} id - El ID a asignarle a el atributo 'data-id'.
- * @return {void} La función no devuelve ningun valor.
+ * @param {HTMLImageElement} icono_personaje - El elemento icono_personaje del cual se obtendrá el ID.
+ * @return {number} El ID del personaje.
  */
 export const obtener_id_personaje = (icono_personaje) => {
   return parseInt(icono_personaje.getAttribute("data-id"));
@@ -27,7 +33,7 @@ export const obtener_id_personaje = (icono_personaje) => {
 /**
  * Actualiza el contenedor con los datos del personaje proporcionados.
  *
- * @param {HTMLElement} contenedor - El elemento contenedor que se actualizará.
+ * @param {HTMLDivElement} contenedor - El elemento contenedor que se actualizará.
  * @param {Object} datos_personaje - Los datos del personaje a mostrar.
  * @return {void} Esta función no devuelve un valor.
  */
@@ -40,23 +46,33 @@ export const actualizar_contenedor_personaje = (contenedor, personaje) => {
 };
 
 /**
- * Setea los query strings para la ruta dada and con los IDs de personajes, y actualiza el href del botón.
- *
- * @param {HTMLElement} boton - El boton a actualizar.
- * @param {string} route - La ruta base para construir la URL.
- * @param {number} id_personaje_1 - El ID del primer personaje.
- * @param {number} id_personaje_2 - El ID del segundo personaje.
- * @return {void} La función no devuelve ningun valor.
+ * Navega a una nueva página con los parámetros de los personajes seleccionados.
+ * 
+ * @param {string} route - La ruta base de la página a la que se quiere navegar.
+ * @param {string} id_personaje_1 - El ID del primer personaje.
+ * @param {string} id_personaje_2 - El ID del segundo personaje.
  */
 export const navegar_pagina = (route, id_personaje_1, id_personaje_2) => {
   const url = `${route}?personaje_1=${id_personaje_1}&personaje_2=${id_personaje_2}`;
   location.assign(url);
 };
 
+/**
+ * Actualiza el texto de un contenedor.
+ * 
+ * @param {HTMLElement} contenedor - El elemento HTML cuyo texto se va a actualizar.
+ * @param {string} texto - El nuevo texto a establecer en el contenedor.
+ */
 export const actualizar_texto = (contenedor, texto) => {
   contenedor.innerText = texto;
 };
 
+/**
+ * Marca un personaje como seleccionado en una grilla de personajes.
+ * 
+ * @param {HTMLDivElement} grilla_personajes - El contenedor de la grilla de personajes.
+ * @param {string} id_personaje_elegido - El ID del personaje a marcar como seleccionado.
+ */
 export const marcar_seleccion = (grilla_personajes, id_personaje_elegido) => {
   const icono_personaje = grilla_personajes.querySelector(
     `[data-id="${id_personaje_elegido}"]`
@@ -64,22 +80,37 @@ export const marcar_seleccion = (grilla_personajes, id_personaje_elegido) => {
   icono_personaje.classList.add("grillaPersonajes__thumbnail--selected");
 };
 
-export const desmarcar_seleccion = (
-  grilla_personajes,
-  id_personaje_elegido
-) => {
+/**
+ * Desmarca un personaje como seleccionado en una grilla de personajes.
+ * 
+ * @param {HTMLDivElement} grilla_personajes - El contenedor de la grilla de personajes.
+ * @param {string} id_personaje_elegido - El ID del personaje a desmarcar como seleccionado.
+ */
+export const desmarcar_seleccion = (grilla_personajes, id_personaje_elegido) => {
   const icono_personaje = grilla_personajes.querySelector(
     `[data-id="${id_personaje_elegido}"]`
   );
   icono_personaje.classList.remove("grillaPersonajes__thumbnail--selected");
 };
 
+/**
+ * Deshabilita un botón.
+ * 
+ * @param {HTMLButtonElement} boton - El botón a deshabilitar.
+ */
 export const deshabilitar_boton = (boton) => {
   boton.disabled = true;
 };
+
+/**
+ * Habilita un botón.
+ * 
+ * @param {HTMLButtonElement} boton - El botón a habilitar.
+ */
 export const habilitar_boton = (boton) => {
   boton.disabled = false;
 };
+
 
 export const fetch_personajes = async () => {
   try {
